e0114e1f8c7670063843be744d3a935f4579e173
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2019-03-05 20:39:18 -0300

    Detalles de rutas

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -17,17 +17,12 @@ class WishlistsController < ApplicationController
     respond_to do |format|
       format.js { flash[:info] = 'Has agregado producto a tu lista de deseos' }
     end
-
-    # flash[:success] =  'Has agregado producto a tu lista de deseos'
-    # respond_to :js
-      
-    # redirect_to root_path
   end
 
   def destroy
     @detail = Detail.find(params[:detail_id])
     @whishlist = Wishlist.where(detail: @detail, user: current_user).first
-    byebug
+    
     respond_to do |format|
       if @whishlist.destroy
         flash[:warning] = 'Has quitado producto de tu lista de deseos.'
