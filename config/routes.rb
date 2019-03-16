6faf55957f7eed92a6e6fcc056a5acc156d67602
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2019-03-16 11:23:23 -0300

    Ajuste de en navbar y similares

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :gifts 
-  
+  resources :gifts
   resources :claims do
     collection do
       post 'execute'
@@ -13,6 +12,7 @@ Rails.application.routes.draw do
   resources :addresses
   resources :authors
   resources :banners
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
